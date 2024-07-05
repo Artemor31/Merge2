98a4c86b2fb63316f1670d6adedad25c24ab4107
Artyom Bogunov <bogunovartyom@yandex.ru>
2024-07-05 18:28:02 +0300

    added level label

--- Assets/CodeBase/Gameplay/Units/Healthbar.cs
+++ Assets/CodeBase/Gameplay/Units/Healthbar.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CodeBase.Infrastructure;
 using CodeBase.LevelData;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,19 +10,26 @@ namespace CodeBase.Gameplay.Units
     public class Healthbar : MonoBehaviour
     {
         [SerializeField] private Image _value;
+        [SerializeField] private TextMeshProUGUI _level;
         [SerializeField] private Health _health;
+        [SerializeField] private Actor _actor;
+        [SerializeField] private Transform _target;
+        [SerializeField] private Vector3 _offset;
         [SerializeField] private Canvas _canvas;
-        private Transform _lookAt;
 
         private void OnEnable()
         {
             SetValue();
             _health.HealthChanged += SetValue;
-            _canvas.worldCamera = ServiceLocator.Resolve<CameraService>().CurrentMainCamera();
-            _lookAt = _canvas.worldCamera.transform;
+            _level.text = _actor.Level.ToString();
+            
+            Camera currentCamera = ServiceLocator.Resolve<CameraService>().CurrentMainCamera();
+            _canvas.worldCamera = currentCamera;
+            transform.LookAt(-currentCamera.transform.position);
         }
 
-        private void Update() => transform.LookAt(_lookAt.position);
+        // separate spawning of actor and attaching healthbar to him
+        private void Update() => transform.position = _target.position + _offset;
         private void OnDisable() => _health.HealthChanged -= SetValue;
         private void SetValue() => _value.fillAmount = _health.Ratio;
     }
