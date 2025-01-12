0b652a2a0ad794e52961ddf02f40dfa30816a05c
a.bogunov <bogunov_aa@edu.bstu.ru>
2025-01-12 11:00:05 +0300

    added prefs viewer

---
+++ Assets/ImportedAssets/Code/PlayerPrefsEditor/Editor/PreferencesEditor/PreferenceEntry.cs
@@ -0,0 +1,30 @@
+﻿namespace BgTools.PlayerPrefsEditor
+{
+    [System.Serializable]
+    public class PreferenceEntry
+    {
+        public enum PrefTypes
+        {
+            String = 0,
+            Int = 1,
+            Float = 2
+        }
+
+        public PrefTypes m_typeSelection;
+        public string m_key;
+        public string m_strValue;
+        public int m_intValue;
+        public float m_floatValue;
+
+        public string ValueAsString()
+        {
+            return m_typeSelection switch
+            {
+                PrefTypes.String => m_strValue,
+                PrefTypes.Int => m_intValue.ToString(),
+                PrefTypes.Float => m_floatValue.ToString(),
+                _ => string.Empty
+            };
+        }
+    }
+}
---
+++ Assets/ImportedAssets/Code/PlayerPrefsEditor/Editor/PreferencesEditor/PreferenceEntryHolder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BgTools.PlayerPrefsEditor
+{
+    [System.Serializable]
+    public class PreferenceEntryHolder : ScriptableObject
+    {
+        public List<PreferenceEntry> userDefList;
+        public List<PreferenceEntry> unityDefList;
+
+        private void OnEnable()
+        {
+            hideFlags = HideFlags.DontSave;
+            userDefList ??= new List<PreferenceEntry>();
+            unityDefList ??= new List<PreferenceEntry>();
+        }
+
+        public void ClearLists()
+        {
+            userDefList?.Clear();
+            unityDefList?.Clear();
+        }
+    }
+}
